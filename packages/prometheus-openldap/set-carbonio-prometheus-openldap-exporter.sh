37585ffb851b3ff1383ee8ae5d8ff06dd2905c87
Lorenzo Fascì <lorenzo.fasci@zextras.com>
2022-11-23 06:39:18 +0100

    Fixed syntax

--- packages/prometheus-openldap/set-carbonio-prometheus-openldap-exporter.sh
+++ packages/prometheus-openldap/set-carbonio-prometheus-openldap-exporter.sh
@@ -5,6 +5,6 @@ source /opt/zextras/bin/zmshutil || exit 1
 zmsetvars
 
 echo "ldapAddr: $ldap_host:$ldap_port" > /opt/zextras/common/etc/prometheus/$pkgname.yml
-echo "ldapUser: $zimbra_ldap_userdn >> /opt/zextras/common/etc/prometheus/$pkgname.yml
-echo "ldapPass: $zimbra_ldap_password  >> /opt/zextras/common/etc/prometheus/$pkgname.yml
+echo "ldapUser: $zimbra_ldap_userdn" >> /opt/zextras/common/etc/prometheus/$pkgname.yml
+echo "ldapPass: $zimbra_ldap_password"  >> /opt/zextras/common/etc/prometheus/$pkgname.yml
 echo "interval: 1m" >> /opt/zextras/common/etc/prometheus/$pkgname.yml
