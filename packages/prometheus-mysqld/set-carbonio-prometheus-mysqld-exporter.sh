dc217b828d03c518e3c641889f9ec23eeb861246
Lorenzo Fascì <lorenzo.fasci@zextras.com>
2022-11-23 07:12:11 +0100

    Fixed mysql, added again all packages

--- packages/prometheus-mysqld/set-carbonio-prometheus-mysqld-exporter.sh
+++ packages/prometheus-mysqld/set-carbonio-prometheus-mysqld-exporter.sh
@@ -5,6 +5,6 @@ source /opt/zextras/bin/zmshutil || exit 1
 zmsetvars
 
 echo "user= $zimbra_mysql_user" >> /opt/zextras/common/etc/prometheus/$pkgname.cnf
-echo "password= $mysql_zimbra_password" >> /opt/zextras/common/etc/prometheus/$pkgname.cnf
+echo "password= $zimbra_mysql_password" >> /opt/zextras/common/etc/prometheus/$pkgname.cnf
 echo "host= $mysql_bind_address"  >> /opt/zextras/common/etc/prometheus/$pkgname.cnf
 echo "port= $mysql_port" >> /opt/zextras/common/etc/prometheus/$pkgname.cnf
