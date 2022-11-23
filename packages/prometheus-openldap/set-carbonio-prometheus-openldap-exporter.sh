0c45ee2d5c2216deca7b95526b6f8e44afcc4030
Lorenzo Fascì <lorenzo.fasci@zextras.com>
2022-11-23 06:33:49 +0100

    Setup script for openldap

---
+++ packages/prometheus-openldap/set-carbonio-prometheus-openldap-exporter.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+pkgname="carbonio-prometheus-mysqld-exporter"
+source /opt/zextras/bin/zmshutil || exit 1
+zmsetvars
+
+echo "ldapAddr: $ldap_host:$ldap_port" > /opt/zextras/common/etc/prometheus/$pkgname.yml
+echo "ldapUser: $zimbra_ldap_userdn >> /opt/zextras/common/etc/prometheus/$pkgname.yml
+echo "ldapPass: $zimbra_ldap_password  >> /opt/zextras/common/etc/prometheus/$pkgname.yml
+echo "interval: 1m" >> /opt/zextras/common/etc/prometheus/$pkgname.yml
