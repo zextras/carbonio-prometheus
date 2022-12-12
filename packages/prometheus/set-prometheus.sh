62b7144500ba686dbd0f74fd5bc63378aa1c23dd
Lorenzo Fascì <lorenzo.fasci@zextras.com>
2022-12-12 11:30:35 +0100

    Added print error message for wrong password or comand failure

--- packages/prometheus/set-prometheus.sh
+++ packages/prometheus/set-prometheus.sh
@@ -2,16 +2,23 @@
 # Read Password
 echo -n Please type Service Discover password: 
 read -s password
-export  CONSUL_HTTP_TOKEN=$(echo $password | gpg --batch --yes --passphrase-fd 0 -qdo - /etc/zextras/service-discover/cluster-credentials.tar.gpg | tar xOf - consul-acl-secret.json | jq .SecretID -r);
 
-sed -i s/"{{ consultoken }}"/$CONSUL_HTTP_TOKEN/g /etc/carbonio/carbonio-prometheus/prometheus.yml;
-dom=$(hostname -d);
-sed -i s/"{{ hostsdomain }}"/$dom/g /etc/carbonio/carbonio-prometheus/prometheus.yml;
-consuldom=$(echo $(hostname -d) | sed s/\\\./-/g);
-sed -i s/"{{ consulhostsdomain }}"/$consuldom/g /etc/carbonio/carbonio-prometheus/prometheus.yml;
+CONSUL_HTTP_TOKEN=$(echo $password | gpg --batch --yes --passphrase-fd 0 -qdo - /etc/zextras/service-discover/cluster-credentials.tar.gpg | tar xOf - consul-acl-secret.json | jq .SecretID -r);
 
-echo "Restarting Carbonio Prometheus Service"
-systemctl restart carbonio-prometheus
+if [ -z ${CONSUL_HTTP_TOKEN} ]; then 
+    echo "Wrong password";
+    exit 1;
+else
+    export CONSUL_HTTP_TOKEN
+    sed -i s/"{{ consultoken }}"/$CONSUL_HTTP_TOKEN/g /etc/carbonio/carbonio-prometheus/prometheus.yml;
+    dom=$(hostname -d);
+    sed -i s/"{{ hostsdomain }}"/$dom/g /etc/carbonio/carbonio-prometheus/prometheus.yml;
+    consuldom=$(echo $(hostname -d) | sed s/\\\./-/g);
+    sed -i s/"{{ consulhostsdomain }}"/$consuldom/g /etc/carbonio/carbonio-prometheus/prometheus.yml;
 
-echo "Reloading Service Discover"
-consul reload
+    echo "Restarting Carbonio Prometheus Service"
+    systemctl restart carbonio-prometheus
+
+    echo "Reloading Service Discover"
+    consul reload
+fi 
