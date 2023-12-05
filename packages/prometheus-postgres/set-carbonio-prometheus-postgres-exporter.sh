0739eaa52178e69abcc3f09bb8f7790fe7d9b09b
Mary Tishchenkova <mary.tishchenkova@zextras.com>
2023-12-05 17:05:55 +0100

    change versions and fix pgpool

--- packages/prometheus-postgres/set-carbonio-prometheus-postgres-exporter.sh
+++ packages/prometheus-postgres/set-carbonio-prometheus-postgres-exporter.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-su - postgres -c "psql --command='create USER \"carbonio-prometheus\"  password \"carbonio-prometheus\";'"
-su - postgres -c "psql --command='ALTER USER \"carbonio-prometheus\" SET SEARCH_PATH TO postgres_exporter,pg_catalog;'"
-su - postgres -c "psql --command='GRANT CONNECT ON DATABASE postgres TO \"carbonio-prometheus\";'"
-su - postgres -c "psql --command='GRANT pg_monitor to \"carbonio-prometheus\";'"
+# su - postgres -c "psql --command='create USER \"carbonio-prometheus\"  password \"carbonio-prometheus\";'"
+# su - postgres -c "psql --command='ALTER USER \"carbonio-prometheus\" SET SEARCH_PATH TO postgres_exporter,pg_catalog;'"
+# su - postgres -c "psql --command='GRANT CONNECT ON DATABASE postgres TO \"carbonio-prometheus\";'"
+# su - postgres -c "psql --command='GRANT pg_monitor to \"carbonio-prometheus\";'"
+
+su - postgres -c "psql --command=\"create USER carbonio_prometheus PASSWORD 'mb63Q0sV';\""
+su - postgres -c "psql --command=\"ALTER USER carbonio_prometheus SET SEARCH_PATH TO postgres_exporter,pg_catalog;\""
+su - postgres -c "psql --command=\"GRANT CONNECT ON DATABASE postgres TO carbonio_prometheus;\""
+su - postgres -c "psql --command=\"GRANT pg_monitor to carbonio_prometheus;\""
