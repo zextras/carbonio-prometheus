192698f3477bdd8914747173ac48dec1862b1bc6
Mary Tishchenkova <mary.tishchenkova@zextras.com>
2024-02-13 09:10:34 +0100

    chore: change script

--- packages/prometheus-mysqld/set-carbonio-prometheus-mysqld-exporter.sh
+++ packages/prometheus-mysqld/set-carbonio-prometheus-mysqld-exporter.sh
@@ -4,8 +4,12 @@ pkgname="carbonio-prometheus-mysqld-exporter"
 #mkdir -p /etc/carbonio/carbonio-prometheus-mysqld-exporter/
 
 # shellcheck source=/dev/null
-source /opt/zextras/bin/zmshutil || exit 1
-zmsetvars
+# source /opt/zextras/bin/zmshutil || exit 1
+# zmsetvars
+zimbra_mysql_user=$(/opt/zextras/bin/zmlocalconfig -s -m nokey zimbra_mysql_user)
+zimbra_mysql_password=$(/opt/zextras/bin/zmlocalconfig -s -m nokey zimbra_mysql_password)
+mysql_bind_address=$(/opt/zextras/bin/zmlocalconfig -s -m nokey mysql_bind_address)
+mysql_port=$(/opt/zextras/bin/zmlocalconfig -s -m nokey mysql_port)
 
 {
   echo "[client]"
