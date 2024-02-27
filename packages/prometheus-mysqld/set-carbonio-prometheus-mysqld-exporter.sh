9a64a2b090343218b4e33d335273865114dd4a32
Mary Tishchenkova <mary.tishchenkova@zextras.com>
2024-02-27 18:40:11 +0100

    set up pending-setup command for mysqld exporter

--- packages/prometheus-mysqld/set-carbonio-prometheus-mysqld-exporter.sh
+++ packages/prometheus-mysqld/set-carbonio-prometheus-mysqld-exporter.sh
@@ -1,20 +1,2 @@
 #!/bin/bash
-
-pkgname="carbonio-prometheus-mysqld-exporter"
-#mkdir -p /etc/carbonio/carbonio-prometheus-mysqld-exporter/
-
-# shellcheck source=/dev/null
-# source /opt/zextras/bin/zmshutil || exit 1
-# zmsetvars
-zimbra_mysql_user=$(/opt/zextras/bin/zmlocalconfig -s -m nokey zimbra_mysql_user)
-zimbra_mysql_password=$(/opt/zextras/bin/zmlocalconfig -s -m nokey zimbra_mysql_password)
-mysql_bind_address=$(/opt/zextras/bin/zmlocalconfig -s -m nokey mysql_bind_address)
-mysql_port=$(/opt/zextras/bin/zmlocalconfig -s -m nokey mysql_port)
-
-{
-  echo "[client]"
-  echo "user= $zimbra_mysql_user"
-  echo "password= $zimbra_mysql_password"
-  echo "host= $mysql_bind_address"
-  echo "port= $mysql_port"
-} >/etc/carbonio/$pkgname/$pkgname.cnf
+carbonio-prometheus-mysqld-exporter-setup setup
