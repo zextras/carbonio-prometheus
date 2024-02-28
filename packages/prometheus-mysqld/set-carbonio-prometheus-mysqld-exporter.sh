fc925600ed04a3710ae865eca80e297272f94111
Mary Tishchenkova <mary.tishchenkova@zextras.com>
2024-02-28 17:11:10 +0100

    work on organization

--- packages/prometheus-mysqld/set-carbonio-prometheus-mysqld-exporter.sh
+++ packages/prometheus-mysqld/set-carbonio-prometheus-mysqld-exporter.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
-carbonio-prometheus-mysqld-exporter-setup setup
+
+pkgname="carbonio-prometheus-mysqld-exporter"
+
+zimbra_mysql_user=$(/opt/zextras/bin/zmlocalconfig -s -m nokey zimbra_mysql_user)
+zimbra_mysql_password=$(/opt/zextras/bin/zmlocalconfig -s -m nokey zimbra_mysql_password)
+mysql_bind_address=$(/opt/zextras/bin/zmlocalconfig -s -m nokey mysql_bind_address)
+mysql_port=$(/opt/zextras/bin/zmlocalconfig -s -m nokey mysql_port)
+
+{
+  echo "[client]"
+  echo "user= $zimbra_mysql_user"
+  echo "password= $zimbra_mysql_password"
+  echo "host= $mysql_bind_address"
+  echo "port= $mysql_port"
+} >/etc/carbonio/$pkgname/$pkgname.cnf
+
+systemctl restart carbonio-prometheus-mysqld-exporter.service
