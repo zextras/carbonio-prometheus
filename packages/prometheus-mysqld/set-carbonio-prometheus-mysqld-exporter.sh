2fe77b8b11a2577d481bb86688fd54f2af8e2b99
Mary Tishchenkova <mary.tishchenkova@zextras.com>
2024-03-20 15:42:17 +0100

    fix: some  enhancements for mysql exporter (#14)

--- packages/prometheus-mysqld/set-carbonio-prometheus-mysqld-exporter.sh
+++ packages/prometheus-mysqld/set-carbonio-prometheus-mysqld-exporter.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 pkgname="carbonio-prometheus-mysqld-exporter"
-mkdir -p /etc/carbonio/carbonio-prometheus-mysqld-exporter/
 
-# shellcheck source=/dev/null
-source /opt/zextras/bin/zmshutil || exit 1
-zmsetvars
+mysql_user=$(/opt/zextras/bin/zmlocalconfig -s -m nokey zimbra_mysql_user)
+mysql_password=$(/opt/zextras/bin/zmlocalconfig -s -m nokey zimbra_mysql_password)
+mysql_bind_address=$(/opt/zextras/bin/zmlocalconfig -s -m nokey mysql_bind_address)
+mysql_port=$(/opt/zextras/bin/zmlocalconfig -s -m nokey mysql_port)
 
 {
   echo "[client]"
-  echo "user= $zimbra_mysql_user"
-  echo "password= $zimbra_mysql_password"
+  echo "user= $mysql_user"
+  echo "password= $mysql_password"
   echo "host= $mysql_bind_address"
   echo "port= $mysql_port"
 } >/etc/carbonio/$pkgname/$pkgname.cnf
+
+systemctl restart carbonio-prometheus-mysqld-exporter.service
--- packages/prometheus-openldap/set-carbonio-prometheus-openldap-exporter.sh
+++ packages/prometheus-openldap/set-carbonio-prometheus-openldap-exporter.sh
@@ -3,13 +3,13 @@
 pkgname="carbonio-prometheus-openldap-exporter"
 ldap_host=$(/opt/zextras/bin/zmlocalconfig -s -m nokey ldap_host)
 ldap_port=$(/opt/zextras/bin/zmlocalconfig -s -m nokey ldap_port)
-zimbra_ldap_userdn=$(/opt/zextras/bin/zmlocalconfig -s -m nokey zimbra_ldap_userdn)
-zimbra_ldap_password=$(/opt/zextras/bin/zmlocalconfig -s -m nokey zimbra_ldap_password)
+ldap_userdn=$(/opt/zextras/bin/zmlocalconfig -s -m nokey zimbra_ldap_userdn)
+ldap_password=$(/opt/zextras/bin/zmlocalconfig -s -m nokey zimbra_ldap_password)
 
 {
   echo "ldapAddr: $ldap_host:$ldap_port"
-  echo "ldapUser: $zimbra_ldap_userdn"
-  echo "ldapPass: $zimbra_ldap_password"
+  echo "ldapUser: $ldap_userdn"
+  echo "ldapPass: $ldap_password"
   echo "interval: 1m"
 } >/etc/carbonio/${pkgname}/$pkgname.yml
 
